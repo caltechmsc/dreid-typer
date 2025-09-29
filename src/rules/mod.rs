0e152f1154cbe20a0cc0888245b9fbe51219697c
Tony Kan <tonykan@caltech.edu>
2025-09-28 22:05:30 -0700

    feat(rules): Add Conditions struct with optional fields and custom deserialization

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -33,3 +33,38 @@ where
         })
         .collect()
 }
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq, Default)]
+#[serde(deny_unknown_fields)]
+pub struct Conditions {
+    #[serde(default, deserialize_with = "from_str_optional")]
+    pub element: Option<Element>,
+    #[serde(default)]
+    pub degree: Option<u8>,
+    #[serde(default, deserialize_with = "from_str_optional")]
+    pub hybridization: Option<Hybridization>,
+    #[serde(default)]
+    pub is_in_ring: Option<bool>,
+    #[serde(default)]
+    pub is_aromatic: Option<bool>,
+    #[serde(default)]
+    pub smallest_ring_size: Option<u8>,
+
+    #[serde(default, deserialize_with = "hash_map_from_str_keys")]
+    pub neighbor_elements: HashMap<Element, u8>,
+    #[serde(default)]
+    pub neighbor_types: HashMap<String, u8>,
+}
+
+fn from_str_optional<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    T: FromStr,
+    T::Err: fmt::Display,
+    D: Deserializer<'de>,
+{
+    let opt_s = Option::<String>::deserialize(deserializer)?;
+    match opt_s {
+        Some(s) => T::from_str(&s).map(Some).map_err(de::Error::custom),
+        None => Ok(None),
+    }
+}
