8944d0849b2920e592a6b46a0ff2e475f38309f7
Tony Kan <tonykan@caltech.edu>
2025-10-04 13:59:19 -0700

    feat(processor): Implement calculate_provisional_hybridization function for hybridization state determination

--- src/processor/hybridization.rs
+++ src/processor/hybridization.rs
@@ -7,3 +7,36 @@ pub(super) enum ProvisionalHybridization {
     SP2,
     SP3,
 }
+
+pub(super) fn calculate_provisional_hybridization(
+    atom: &AtomView,
+    graph: &ProcessingGraph,
+) -> ProvisionalHybridization {
+    let pi_electrons_from_bonds = graph.adjacency[atom.id]
+        .iter()
+        .map(|(_, order)| order.pi_contribution())
+        .sum::<u8>();
+
+    let mut prov_hyb = match pi_electrons_from_bonds {
+        2.. => ProvisionalHybridization::SP,
+        1 => ProvisionalHybridization::SP2,
+        0 => ProvisionalHybridization::SP3,
+    };
+
+    if prov_hyb == ProvisionalHybridization::SP3 {
+        match atom.element {
+            Element::N | Element::P | Element::As if atom.degree == 3 => {
+                prov_hyb = ProvisionalHybridization::SP2;
+            }
+            Element::O | Element::S | Element::Se if atom.degree == 2 => {
+                prov_hyb = ProvisionalHybridization::SP2;
+            }
+            Element::B if atom.degree == 3 => {
+                prov_hyb = ProvisionalHybridization::SP2;
+            }
+            _ => {}
+        }
+    }
+
+    prov_hyb
+}
