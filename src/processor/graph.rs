bdcc60f9ff1acaee68b953ddaf6de502a148421a
Tony Kan <tonykan@caltech.edu>
2025-10-04 13:32:57 -0700

    test(processor): Add unit tests for ProcessingGraph initialization and validation

--- src/processor/graph.rs
+++ src/processor/graph.rs
@@ -60,3 +60,88 @@ impl ProcessingGraph {
         Ok(Self { atoms, adjacency })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::graph::MolecularGraph;
+    use crate::core::{BondOrder, Element};
+
+    #[test]
+    fn new_processing_graph_for_methane_is_correct() {
+        let mut mg = MolecularGraph::new();
+        let c1 = mg.add_atom(Element::C);
+        let h1 = mg.add_atom(Element::H);
+        let h2 = mg.add_atom(Element::H);
+        let h3 = mg.add_atom(Element::H);
+        let h4 = mg.add_atom(Element::H);
+        mg.add_bond(c1, h1, BondOrder::Single).unwrap();
+        mg.add_bond(c1, h2, BondOrder::Single).unwrap();
+        mg.add_bond(c1, h3, BondOrder::Single).unwrap();
+        mg.add_bond(c1, h4, BondOrder::Single).unwrap();
+
+        let pg = ProcessingGraph::new(&mg).unwrap();
+
+        assert_eq!(pg.atoms.len(), 5);
+        assert_eq!(pg.adjacency.len(), 5);
+
+        let carbon = &pg.atoms[c1];
+        assert_eq!(carbon.id, c1);
+        assert_eq!(carbon.element, Element::C);
+        assert_eq!(carbon.degree, 4);
+        assert_eq!(carbon.hybridization, Hybridization::Unknown);
+        assert!(!carbon.is_in_ring);
+        assert!(!carbon.is_aromatic);
+        assert_eq!(carbon.smallest_ring_size, None);
+
+        let hydrogen = &pg.atoms[h1];
+        assert_eq!(hydrogen.degree, 1);
+
+        assert_eq!(pg.adjacency[c1].len(), 4);
+        assert_eq!(pg.adjacency[h1].len(), 1);
+        assert_eq!(pg.adjacency[h1][0], (c1, BondOrder::Single));
+    }
+
+    #[test]
+    fn new_processing_graph_for_ethene_is_correct() {
+        let mut mg = MolecularGraph::new();
+        let c1 = mg.add_atom(Element::C);
+        let c2 = mg.add_atom(Element::C);
+        mg.add_bond(c1, c2, BondOrder::Double).unwrap();
+
+        let pg = ProcessingGraph::new(&mg).unwrap();
+        assert_eq!(pg.atoms.len(), 2);
+
+        assert_eq!(pg.atoms[c1].degree, 1);
+        assert_eq!(pg.atoms[c2].degree, 1);
+
+        assert_eq!(pg.adjacency[c1][0], (c2, BondOrder::Double));
+        assert_eq!(pg.adjacency[c2][0], (c1, BondOrder::Double));
+    }
+
+    #[test]
+    fn new_processing_graph_with_invalid_bond_returns_error() {
+        let mut mg = MolecularGraph::new();
+        mg.add_atom(Element::C);
+        mg.add_bond(0, 1, BondOrder::Single).unwrap_err();
+
+        let bad_mg = MolecularGraph {
+            atoms: vec![crate::core::graph::AtomNode {
+                id: 0,
+                element: Element::C,
+            }],
+            bonds: vec![crate::core::graph::BondEdge {
+                id: 0,
+                atom_ids: (0, 1),
+                order: BondOrder::Single,
+            }],
+        };
+
+        let result = ProcessingGraph::new(&bad_mg);
+        assert!(result.is_err());
+        match result.unwrap_err() {
+            GraphValidationError::MissingAtom { id } => assert_eq!(id, 1),
+            _ => panic!("Expected MissingAtom error"),
+        }
+    }
+}
