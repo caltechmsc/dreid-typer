26e2872df9c5d2cedd3c51779905156be50386b7
Tony Kan <tonykan@caltech.edu>
2025-10-12 14:29:30 -0700

    feat(processor): Export AtomView in addition to ProcessingGraph from graph module

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -4,6 +4,6 @@ mod pipeline;
 mod templates;
 mod typer;
 
-pub(crate) use graph::ProcessingGraph;
+pub(crate) use graph::{AtomView, ProcessingGraph};
 pub(crate) use pipeline::perceive;
 pub(crate) use typer::assign_types;
