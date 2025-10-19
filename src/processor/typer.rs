7d8c3e5d0dd27f99f492339530d94699ec492139
Tony Kan <tonykan@caltech.edu>
2025-10-18 20:38:35 -0700

    test(processor): Remove redundant formal charge assignments in atom addition for clarity

--- src/processor/typer.rs
+++ src/processor/typer.rs
@@ -337,11 +337,11 @@ mod tests {
     #[test]
     fn test_methane_typing() {
         let mut mg = MolecularGraph::new();
-        let c1 = mg.add_atom(Element::C, 0);
-        let h1 = mg.add_atom(Element::H, 0);
-        let h2 = mg.add_atom(Element::H, 0);
-        let h3 = mg.add_atom(Element::H, 0);
-        let h4 = mg.add_atom(Element::H, 0);
+        let c1 = mg.add_atom(Element::C);
+        let h1 = mg.add_atom(Element::H);
+        let h2 = mg.add_atom(Element::H);
+        let h3 = mg.add_atom(Element::H);
+        let h4 = mg.add_atom(Element::H);
 
         for &hydrogen in &[h1, h2, h3, h4] {
             mg.add_bond(c1, hydrogen, BondOrder::Single).unwrap();
@@ -357,12 +357,12 @@ mod tests {
     #[test]
     fn test_ethylene_typing() {
         let mut mg = MolecularGraph::new();
-        let c1 = mg.add_atom(Element::C, 0);
-        let c2 = mg.add_atom(Element::C, 0);
-        let h1 = mg.add_atom(Element::H, 0);
-        let h2 = mg.add_atom(Element::H, 0);
-        let h3 = mg.add_atom(Element::H, 0);
-        let h4 = mg.add_atom(Element::H, 0);
+        let c1 = mg.add_atom(Element::C);
+        let c2 = mg.add_atom(Element::C);
+        let h1 = mg.add_atom(Element::H);
+        let h2 = mg.add_atom(Element::H);
+        let h3 = mg.add_atom(Element::H);
+        let h4 = mg.add_atom(Element::H);
         mg.add_bond(c1, c2, BondOrder::Double).unwrap();
         mg.add_bond(c1, h1, BondOrder::Single).unwrap();
         mg.add_bond(c1, h2, BondOrder::Single).unwrap();
@@ -380,10 +380,10 @@ mod tests {
     #[test]
     fn test_acetylene_typing() {
         let mut mg = MolecularGraph::new();
-        let c1 = mg.add_atom(Element::C, 0);
-        let c2 = mg.add_atom(Element::C, 0);
-        let h1 = mg.add_atom(Element::H, 0);
-        let h2 = mg.add_atom(Element::H, 0);
+        let c1 = mg.add_atom(Element::C);
+        let c2 = mg.add_atom(Element::C);
+        let h1 = mg.add_atom(Element::H);
+        let h2 = mg.add_atom(Element::H);
         mg.add_bond(c1, c2, BondOrder::Triple).unwrap();
         mg.add_bond(c1, h1, BondOrder::Single).unwrap();
         mg.add_bond(c2, h2, BondOrder::Single).unwrap();
@@ -399,7 +399,7 @@ mod tests {
     fn test_benzene_aromatic_typing() {
         let mut mg = MolecularGraph::new();
         for _ in 0..6 {
-            mg.add_atom(Element::C, 0);
+            mg.add_atom(Element::C);
         }
         for i in 0..6 {
             mg.add_bond(i, (i + 1) % 6, BondOrder::Aromatic).unwrap();
@@ -414,10 +414,10 @@ mod tests {
     #[test]
     fn test_halogens_and_ions_typing() {
         let mut mg = MolecularGraph::new();
-        let f = mg.add_atom(Element::F, 0);
-        let cl = mg.add_atom(Element::Cl, 0);
-        let br = mg.add_atom(Element::Br, 0);
-        let na = mg.add_atom(Element::Na, 0);
+        let f = mg.add_atom(Element::F);
+        let cl = mg.add_atom(Element::Cl);
+        let br = mg.add_atom(Element::Br);
+        let na = mg.add_atom(Element::Na);
 
         let types = run_typing_test(&mg, None).unwrap();
         assert_eq!(types[f], "F_");
@@ -436,7 +436,7 @@ mod tests {
     #[test]
     fn test_single_atom() {
         let mut mg = MolecularGraph::new();
-        let c = mg.add_atom(Element::C, 0);
+        let c = mg.add_atom(Element::C);
         let types = run_typing_test(&mg, None).unwrap();
         assert_eq!(types[c], "C_1");
     }
@@ -444,8 +444,8 @@ mod tests {
     #[test]
     fn test_diatomic_oxygen() {
         let mut mg = MolecularGraph::new();
-        let o1 = mg.add_atom(Element::O, 0);
-        let o2 = mg.add_atom(Element::O, 0);
+        let o1 = mg.add_atom(Element::O);
+        let o2 = mg.add_atom(Element::O);
         mg.add_bond(o1, o2, BondOrder::Double).unwrap();
 
         let types = run_typing_test(&mg, None).unwrap();
@@ -456,14 +456,14 @@ mod tests {
     #[test]
     fn test_acetic_acid_relaxation_logic() {
         let mut mg = MolecularGraph::new();
-        let c_me = mg.add_atom(Element::C, 0);
-        let c_co = mg.add_atom(Element::C, 0);
-        let o_co = mg.add_atom(Element::O, 0);
-        let o_oh = mg.add_atom(Element::O, 0);
-        let h_oh = mg.add_atom(Element::H, 0);
-        let h_me1 = mg.add_atom(Element::H, 0);
-        let h_me2 = mg.add_atom(Element::H, 0);
-        let h_me3 = mg.add_atom(Element::H, 0);
+        let c_me = mg.add_atom(Element::C);
+        let c_co = mg.add_atom(Element::C);
+        let o_co = mg.add_atom(Element::O);
+        let o_oh = mg.add_atom(Element::O);
+        let h_oh = mg.add_atom(Element::H);
+        let h_me1 = mg.add_atom(Element::H);
+        let h_me2 = mg.add_atom(Element::H);
+        let h_me3 = mg.add_atom(Element::H);
 
         mg.add_bond(c_me, c_co, BondOrder::Single).unwrap();
         mg.add_bond(c_me, h_me1, BondOrder::Single).unwrap();
@@ -488,14 +488,14 @@ mod tests {
     #[test]
     fn test_diborane_special_hydrogen() {
         let mut mg = MolecularGraph::new();
-        let b1 = mg.add_atom(Element::B, 0);
-        let b2 = mg.add_atom(Element::B, 0);
-        let h_bridge1 = mg.add_atom(Element::H, 0);
-        let h_bridge2 = mg.add_atom(Element::H, 0);
-        let h_term1a = mg.add_atom(Element::H, 0);
-        let h_term1b = mg.add_atom(Element::H, 0);
-        let h_term2a = mg.add_atom(Element::H, 0);
-        let h_term2b = mg.add_atom(Element::H, 0);
+        let b1 = mg.add_atom(Element::B);
+        let b2 = mg.add_atom(Element::B);
+        let h_bridge1 = mg.add_atom(Element::H);
+        let h_bridge2 = mg.add_atom(Element::H);
+        let h_term1a = mg.add_atom(Element::H);
+        let h_term1b = mg.add_atom(Element::H);
+        let h_term2a = mg.add_atom(Element::H);
+        let h_term2b = mg.add_atom(Element::H);
 
         mg.add_bond(b1, h_bridge1, BondOrder::Single).unwrap();
         mg.add_bond(b2, h_bridge1, BondOrder::Single).unwrap();
@@ -524,7 +524,7 @@ mod tests {
     #[test]
     fn test_untypable_element_fails_gracefully() {
         let mut mg = MolecularGraph::new();
-        mg.add_atom(Element::Lr, 0);
+        mg.add_atom(Element::Lr);
 
         let result = run_typing_test(&mg, None);
         assert!(result.is_err());
@@ -540,8 +540,8 @@ mod tests {
     #[test]
     fn test_priority_logic_chooses_higher_priority_rule() {
         let mut mg = MolecularGraph::new();
-        let c1 = mg.add_atom(Element::C, 0);
-        mg.add_atom(Element::H, 0);
+        let c1 = mg.add_atom(Element::C);
+        mg.add_atom(Element::H);
         mg.add_bond(c1, 1, BondOrder::Single).unwrap();
 
         let custom_rules_toml = r#"
