e2fc6257a2ec5ce8faee784c5803d0662e671eae
Tony Kan <tonykan@caltech.edu>
2025-11-13 16:35:30 -0800

    test(perception): Add unit tests for AnnotatedMolecule and AnnotatedAtom structures

--- src/perception/model.rs
+++ src/perception/model.rs
@@ -260,3 +260,133 @@ impl MoleculeGraph for AnnotatedMolecule {
         self.bonds.iter()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::graph::AtomNode;
+    use pauling::traits::{AtomView, BondView, MoleculeGraph};
+
+    fn water_like_graph() -> MolecularGraph {
+        let mut graph = MolecularGraph::new();
+        let oxygen = graph.add_atom(Element::O);
+        let hydrogen1 = graph.add_atom(Element::H);
+        let hydrogen2 = graph.add_atom(Element::H);
+
+        graph
+            .add_bond(oxygen, hydrogen1, BondOrder::Single)
+            .expect("valid bond should be created");
+        graph
+            .add_bond(oxygen, hydrogen2, BondOrder::Single)
+            .expect("valid bond should be created");
+
+        graph
+    }
+
+    #[test]
+    fn annotated_molecule_new_populates_adjacency_and_atoms() {
+        let graph = water_like_graph();
+        let molecule = AnnotatedMolecule::new(&graph).expect("graph should be valid");
+
+        let adjacency_sizes: Vec<_> = molecule
+            .adjacency
+            .iter()
+            .map(|neighbors| neighbors.len())
+            .collect();
+        assert_eq!(adjacency_sizes, vec![2, 1, 1]);
+
+        let oxygen_neighbors = &molecule.adjacency[0];
+        assert!(oxygen_neighbors.contains(&(1, BondOrder::Single)));
+        assert!(oxygen_neighbors.contains(&(2, BondOrder::Single)));
+
+        let oxygen = &molecule.atoms[0];
+        assert_eq!(oxygen.element, Element::O);
+        assert_eq!(oxygen.degree, 2);
+        assert_eq!(oxygen.formal_charge, 0);
+        assert_eq!(oxygen.lone_pairs, 0);
+        assert!(!oxygen.is_in_ring);
+        assert_eq!(oxygen.smallest_ring_size, None);
+        assert!(!oxygen.is_aromatic);
+        assert!(!oxygen.is_anti_aromatic);
+        assert!(!oxygen.is_in_conjugated_system);
+        assert!(!oxygen.is_resonant);
+        assert_eq!(oxygen.steric_number, 0);
+        assert_eq!(oxygen.hybridization, Hybridization::Unknown);
+    }
+
+    #[test]
+    fn annotated_molecule_new_detects_invalid_bond_endpoints() {
+        let graph = MolecularGraph {
+            atoms: vec![AtomNode {
+                id: 0,
+                element: Element::C,
+            }],
+            bonds: vec![BondEdge {
+                id: 0,
+                atom_ids: (0, 2),
+                order: BondOrder::Single,
+            }],
+        };
+
+        let err = AnnotatedMolecule::new(&graph).expect_err("invalid bond must fail");
+
+        match err {
+            GraphValidationError::MissingAtom { atom_id } => assert_eq!(atom_id, 2),
+            other => panic!("unexpected error returned: {other:?}"),
+        }
+    }
+
+    #[test]
+    fn annotated_atom_satisfies_atom_view_contract() {
+        let atom = AnnotatedAtom {
+            id: 7,
+            element: Element::N,
+            formal_charge: -1,
+            lone_pairs: 2,
+            degree: 3,
+            is_in_ring: true,
+            smallest_ring_size: Some(5),
+            is_aromatic: true,
+            is_anti_aromatic: false,
+            is_in_conjugated_system: true,
+            is_resonant: true,
+            steric_number: 3,
+            hybridization: Hybridization::SP2,
+        };
+
+        assert_eq!(atom.id(), 7);
+        assert_eq!(atom.element(), pauling::Element::N);
+        assert_eq!(atom.formal_charge(), -1);
+    }
+
+    #[test]
+    fn bond_edge_satisfies_bond_view_contract() {
+        let bond = BondEdge {
+            id: 3,
+            atom_ids: (9, 4),
+            order: BondOrder::Aromatic,
+        };
+
+        assert_eq!(bond.id(), 3);
+        assert_eq!(bond.start_atom_id(), 9);
+        assert_eq!(bond.end_atom_id(), 4);
+        assert_eq!(bond.order(), pauling::BondOrder::Aromatic);
+    }
+
+    #[test]
+    fn annotated_molecule_molecule_graph_iterators_match_source_graph() {
+        let graph = water_like_graph();
+        let molecule = AnnotatedMolecule::new(&graph).unwrap();
+
+        let atom_ids: Vec<_> = molecule.atoms().map(|atom| atom.id()).collect();
+        assert_eq!(atom_ids, vec![0, 1, 2]);
+
+        let bond_pairs: Vec<_> = molecule
+            .bonds()
+            .map(|bond| (bond.id(), bond.start_atom_id(), bond.end_atom_id()))
+            .collect();
+        assert_eq!(bond_pairs.len(), 2);
+        assert!(bond_pairs.contains(&(0, 0, 1)));
+        assert!(bond_pairs.contains(&(1, 0, 2)));
+    }
+}
