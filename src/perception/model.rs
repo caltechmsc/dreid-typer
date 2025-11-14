a47cbeef7771d1734f49125eefb4f2f1e4ac15cb
Tony Kan <tonykan@caltech.edu>
2025-11-13 16:34:38 -0800

    feat(perception): Implement AnnotatedMolecule structure for enhanced molecular representation

--- src/perception/model.rs
+++ src/perception/model.rs
@@ -22,3 +22,55 @@ pub struct AnnotatedAtom {
     pub steric_number: u8,
     pub hybridization: Hybridization,
 }
+
+#[derive(Debug, Clone)]
+pub struct AnnotatedMolecule {
+    pub atoms: Vec<AnnotatedAtom>,
+    pub bonds: Vec<BondEdge>,
+    pub adjacency: Vec<Vec<(usize, BondOrder)>>,
+}
+
+impl AnnotatedMolecule {
+    pub fn new(graph: &MolecularGraph) -> Result<Self, GraphValidationError> {
+        let mut adjacency = vec![vec![]; graph.atoms.len()];
+        for bond in &graph.bonds {
+            let (u, v) = bond.atom_ids;
+
+            if u >= graph.atoms.len() {
+                return Err(GraphValidationError::MissingAtom { atom_id: u });
+            }
+            if v >= graph.atoms.len() {
+                return Err(GraphValidationError::MissingAtom { atom_id: v });
+            }
+
+            adjacency[u].push((v, bond.order));
+            adjacency[v].push((u, bond.order));
+        }
+
+        let atoms = graph
+            .atoms
+            .iter()
+            .map(|node| AnnotatedAtom {
+                id: node.id,
+                element: node.element,
+                degree: adjacency[node.id].len() as u8,
+                formal_charge: 0,
+                lone_pairs: 0,
+                is_in_ring: false,
+                smallest_ring_size: None,
+                is_aromatic: false,
+                is_anti_aromatic: false,
+                is_in_conjugated_system: false,
+                is_resonant: false,
+                steric_number: 0,
+                hybridization: Hybridization::Unknown,
+            })
+            .collect();
+
+        Ok(Self {
+            atoms,
+            bonds: graph.bonds.clone(),
+            adjacency,
+        })
+    }
+}
