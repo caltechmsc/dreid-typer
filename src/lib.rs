00b481eb99a36a95b78113056f047eeae01e3e11
Tony Kan <tonykan@caltech.edu>
2025-11-30 15:51:10 -0800

    fix(core): Add missing property parse error types to public exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ mod core;
 pub use crate::core::error::{AssignmentError, GraphValidationError, PerceptionError, TyperError};
 pub use crate::core::graph::MolecularGraph;
 pub use crate::core::properties::{Element, GraphBondOrder, Hybridization, TopologyBondOrder};
+pub use crate::core::properties::{
+    ParseElementError, ParseGraphBondOrderError, ParseHybridizationError,
+    ParseTopologyBondOrderError,
+};
 pub use crate::core::topology::{
     Angle, Atom, Bond, ImproperDihedral, MolecularTopology, ProperDihedral,
 };
