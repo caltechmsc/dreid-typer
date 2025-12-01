ac92a0e92de238ca811658db22297c413badb006
Tony Kan <tonykan@caltech.edu>
2025-11-30 21:02:09 -0800

    test(typing): Add unit tests for DREIDING typing engine functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod builder;
 mod core;
 mod perception;
+mod typing;
 
 pub use crate::core::error::{AssignmentError, GraphValidationError, PerceptionError, TyperError};
 pub use crate::core::graph::MolecularGraph;
