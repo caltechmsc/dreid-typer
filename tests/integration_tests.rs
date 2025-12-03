ccd397a2bef1cc474ba3b6818756d8edc53adf88
Tony Kan <tonykan@caltech.edu>
2025-12-03 14:03:05 -0800

    test(integration): Add amino acid test cases and integrate into test suite

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -13,3 +13,52 @@ macro_rules! generate_molecule_test {
 }
 
 generate_molecule_test!(glycine_zwitterion_is_typed_correctly, GLYCINE_ZWITTERION);
+generate_molecule_test!(alanine_zwitterion_is_typed_correctly, ALANINE_ZWITTERION);
+generate_molecule_test!(valine_zwitterion_is_typed_correctly, VALINE_ZWITTERION);
+generate_molecule_test!(leucine_zwitterion_is_typed_correctly, LEUCINE_ZWITTERION);
+generate_molecule_test!(
+    isoleucine_zwitterion_is_typed_correctly,
+    ISOLEUCINE_ZWITTERION
+);
+generate_molecule_test!(proline_zwitterion_is_typed_correctly, PROLINE_ZWITTERION);
+generate_molecule_test!(serine_zwitterion_is_typed_correctly, SERINE_ZWITTERION);
+generate_molecule_test!(
+    threonine_zwitterion_is_typed_correctly,
+    THREONINE_ZWITTERION
+);
+generate_molecule_test!(cysteine_zwitterion_is_typed_correctly, CYSTEINE_ZWITTERION);
+generate_molecule_test!(
+    methionine_zwitterion_is_typed_correctly,
+    METHIONINE_ZWITTERION
+);
+generate_molecule_test!(
+    aspartate_zwitterion_is_typed_correctly,
+    ASPARTATE_ZWITTERION
+);
+generate_molecule_test!(
+    asparagine_zwitterion_is_typed_correctly,
+    ASPARAGINE_ZWITTERION
+);
+generate_molecule_test!(
+    glutamate_zwitterion_is_typed_correctly,
+    GLUTAMATE_ZWITTERION
+);
+generate_molecule_test!(
+    glutamine_zwitterion_is_typed_correctly,
+    GLUTAMINE_ZWITTERION
+);
+generate_molecule_test!(lysine_zwitterion_is_typed_correctly, LYSINE_ZWITTERION);
+generate_molecule_test!(arginine_zwitterion_is_typed_correctly, ARGININE_ZWITTERION);
+generate_molecule_test!(
+    phenylalanine_zwitterion_is_typed_correctly,
+    PHENYLALANINE_ZWITTERION
+);
+generate_molecule_test!(tyrosine_zwitterion_is_typed_correctly, TYROSINE_ZWITTERION);
+generate_molecule_test!(
+    histidine_zwitterion_is_typed_correctly,
+    HISTIDINE_ZWITTERION
+);
+generate_molecule_test!(
+    tryptophan_zwitterion_is_typed_correctly,
+    TRYPTOPHAN_ZWITTERION
+);
